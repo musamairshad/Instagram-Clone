25bb510b9509e0d7988bceef0482e8248568c9ed
Muhammad Usama <imusamairshad@gmail.com>
2023-05-14 23:31:35 +0500

    splash_view.dart file is added.

---
+++ lib/views/splash_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class SplashView extends StatelessWidget {
+  const SplashView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Image.asset('assets/images/icon.png',
+        width: 180,
+        height: 180,
+        ),
+      ),
+    );
+  }
+}
